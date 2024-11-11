f793ee2d7b8b0ea55ec362a3145a65239f890907
HlibShutov <u140_hlishu_waw@technischools.com>
2024-11-11 19:39:48 +0100

    git init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use argparse::{ArgumentParser, Store, Collect};
+
+use sigma::cmd_init;
+
 fn main() {
-    println!("Hello, world!");
+    let mut command: String = Default::default();
+    let mut args: Vec<String> = Default::default();
+    {
+        let mut parser = ArgumentParser::new();
+        parser.set_description("Simplified Implementation of Git Mechanics and Architecture");
+            
+        parser.refer(&mut command).add_argument("command", Store, "Command to run");
+        parser.refer(&mut args).add_argument("args", Collect, "Arguments");
+
+        parser.parse_args().unwrap();
+    }
+    println!("command: {command}");
+    println!("args: {args:?}");
+    
+    match command.as_str() {
+        "init" => cmd_init(args),
+        _ => panic!("Unknown command"),
+    }
 }
